fe26a46b3bbdfd7f12e71d620cec35de422e9f49
CodeChd <Drowzyking1126@gmail.com>
2023-02-04 08:53:20 +0800

    Client error fixed

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://capstone-jcytf-ccc1.onrender.com'
+export const API_URL = 'https://capstone-jcytf-ccc1.onrender.com'
 
 export const PER_PAGE = 3   
 
