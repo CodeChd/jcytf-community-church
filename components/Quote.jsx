e152a08665d1423f500e4b294afb6d8fbf08fff8
CodeChd <Drowzyking1126@gmail.com>
2023-02-23 16:09:06 +0800

    added Quote component

--- components/Quote.jsx
+++ components/Quote.jsx
@@ -8,19 +8,21 @@ export default function Quote() {
 
             <div className="inner-container">
 
-                    <div className="img-quote"></div>
-                
+                <div className="img-quote"></div>
 
-                <div className="inner">
-                <FaQuoteLeft className="text-white"/>
+                <div className="content-quote">
 
-                         <h1 className="text-white">Isaiah 41:10 NIV</h1>
-                    <hr />
-                    <p className="text-white">
-                        "So do not fear, for I am with you; do not be dismayed, for I am your God. I will strengthen you and help you; I will uphold you with my righteous right hand.
 
-                    </p>
+                    <div className="inner">
+                        <FaQuoteLeft className="text-white" />
 
+                        <h1 className="text-white">Isaiah 41:10 NIV</h1>
+                        <hr className="hr" />
+                        <p className="text-white quote">
+                            So do not fear, for I am with you; do not be dismayed, for I am your God. I will strengthen you and help you; I will uphold you with my righteous right hand.
+                        </p>
+
+                    </div>
                 </div>
             </div>
 
